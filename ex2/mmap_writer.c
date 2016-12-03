908500c46f2339ad1ec0072ab9519b570750ece0
lgazit <lgazit@apple.com>
2016-12-03 14:17:59 +0200

    lala

--- ex2/mmap_writer.c
+++ ex2/mmap_writer.c
@@ -78,11 +78,11 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-//	// now write to the file as if it were memory
-//	for (i = 0; i < NUM -1; ++i) {
-//		arr[i] = 'a';
-//	}
-//	arr[NUM-1]='\0';
+	// now write to the file as if it were memory
+	for (i = 0; i < NUM -1; ++i) {
+		arr[i] = 'a';
+	}
+	arr[NUM-1]='\0';
 
 	if( gettimeofday(&t2, NULL) < 0){
 		printf("Error getting time: %s\n", strerror(errno));
