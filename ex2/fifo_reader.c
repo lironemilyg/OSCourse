da4738626040f0fcbaba43cb279dbe0cd88f9b42
lgazit <lgazit@apple.com>
2016-12-04 08:35:15 +0200

    lala

--- ex2/fifo_reader.c
+++ ex2/fifo_reader.c
@@ -27,7 +27,7 @@ int main(){
 	char in[READBYTE];
 	int readlen,fd, i,j;
 	bool flag = true;
-	sleep(2);
+	sleep(15);
 	// Time measurement structures
 	struct timeval t1, t2;
 	double elapsed_microsec;
