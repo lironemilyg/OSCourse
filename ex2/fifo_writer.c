6852199b032cd39791afbdc7e09205fff2f9748c
lgazit <lgazit@apple.com>
2016-12-15 18:43:10 +0200

    lala

--- ex2/fifo_writer.c
+++ ex2/fifo_writer.c
@@ -171,7 +171,7 @@ int main(int argc, char* argv[]) {
 	printf("%d were written in %f microseconds through FIFO\n", totalWrite,
 			elapsed_microsec);
 
-	sleep(1);
+//	sleep(1);
 
 	if (close(fd) < 0) {
 		printf("Error close file: %s\n", strerror(errno));
