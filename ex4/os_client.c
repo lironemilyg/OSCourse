44bb620f4f4033751ac34e0b2fe3002d5d413915
lgazit <lgazit@apple.com>
2017-01-13 15:04:20 +0200

    lala

---
+++ ex4/os_client.c
@@ -0,0 +1,124 @@
+/*
+ * os_client.c
+ *
+ *  Created on: Jan 13, 2017
+ *      Author: lirongazit
+ */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 4096
+
+int main(int argc, char *argv[]) {
+	char* ip;
+	short port;
+	int fdsrc, fddst;
+	if (argc != 5) {
+		printf("invalid number of arguments\n");
+		exit(-1);
+	}
+	port = (short) strtol(argv[2], NULL, 10);
+	if (port < 1) {
+		printf("port - invalid arguments\n");
+		exit(-1);
+	}
+	fdsrc = open(argv[3], O_RDONLY);
+	if (fdsrc < 0) {
+		printf("error open() input file %s: %s\n", argv[3], strerror(errno));
+		return -1;
+	}
+	fddst = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fddst < 0) {
+		printf("error open() output file %s: %s\n", argv[4], strerror(errno));
+		return -1;
+	}
+
+	int sockfd = 0, nread = 0;
+	char recvBuff[BUF_SIZE];
+	char sendBuff[BUF_SIZE];
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in my_addr, peer_addr;
+	socklen_t addrsize = sizeof(struct sockaddr_in);
+
+	memset(recvBuff, '0', sizeof(recvBuff));
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Error : Could not create socket \n");
+		return 1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port); // Note: htons for endiannes
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+	printf("connecting...\n");
+	/* Note: what about the client port number? */
+	/* connect socket to the above address */
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
+			< 0) {
+		printf("\n Error : Connect Failed. %s \n", strerror(errno));
+		return 1;
+	}
+
+	bool flag = true;
+	int numsrc, nsent,nwrite,numdst;
+
+	while (flag) {
+		numsrc = read(fdsrc, sendBuff, BUF_SIZE);
+		if (numsrc < 0) {
+			printf("error read() from input file: %s\n", strerror(errno));
+			return -1;
+		}
+		// received EOF - we're done (and that's the ONLY case we're done!)
+		else if (numsrc == 0) {
+			break;
+		}
+		nsent = write(sockfd, sendBuff, numsrc);
+		if (nsent != numsrc) {
+			printf("error occured - write to server \n");
+			return -1;
+		}
+		nread = read(sockfd, recvBuff, sizeof(recvBuff) - 1);
+		if (nread != nsent) {
+			printf("error occured - read from server \n");
+			return -1;
+		}
+		recvBuff[nread] = 0;
+//		if (fputs(recvBuff, stdout) == EOF) {
+//			printf("\n Error : Fputs error\n");
+//		}
+
+		numdst = 0;
+		while (numdst < numsrc) {
+			// ALWAYS look at write()'s return value, don't assume everything is written
+			nwrite = write(fddst, recvBuff + numdst, numsrc - numdst);
+			if (nwrite < 0) {
+				printf("error write() to output file: %s\n", strerror(errno));
+				return -1;
+			}
+
+			// increment our counter
+			numdst += nwrite;
+		}
+	}
+	close(fdsrc);
+	close(fddst);
+	close(sockfd);
+	return 0;
+}
+
---
+++ ex4/os_server.c
@@ -0,0 +1,187 @@
+/*
+ * os_server.c
+ *
+ *  Created on: Jan 13, 2017
+ *      Author: lirongazit
+ */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 4096
+#define RAND_FILE "/dev/urandom"
+
+//taking from - http://stackoverflow.com/questions/2572366/how-to-use-dev-random-or-urandom-in-c
+int create_key_file(int fdkey, int keylen) {
+	int randomData = open(RAND_FILE, O_RDONLY);
+	char myRandomData[BUF_SIZE];
+	size_t randomDataLen = 0;
+	while (randomDataLen < keylen) {
+		ssize_t result = read(randomData, myRandomData + randomDataLen,
+				(sizeof myRandomData) - randomDataLen);
+		if (result < 0) {
+			printf("error reading from %s file\n", RAND_FILE);
+			return -1;
+		}
+		result = write(fdkey, myRandomData, result);
+		if (result < 0) {
+			printf("error writing to key file file\n");
+			return -1;
+		}
+		randomDataLen += result;
+	}
+	close(randomData);
+	return 0;
+}
+
+int xor_buffers(char* srcbuf, int numsrc, char* keyfilename) {
+	int numkey, num, i;
+	int fdkey = open(keyfilename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fdkey < 0) {
+		printf("error open() output file %s: %s\n", keyfilename, strerror(errno));
+		return errno;
+	}
+	char keybuf[BUF_SIZE];
+	// set number of bytes read from the key file to 0
+	numkey = 0;
+	// iterate reading from key until reaching numsrc bytes
+	while (numkey < numsrc) {
+		num = read(fdkey, keybuf + numkey, numsrc - numkey);
+		if (num < 0) {
+			printf("error read() key: %s\n", strerror(errno));
+			exit(errno);
+		} else if (num == 0) {
+			if (lseek(fdkey, SEEK_SET, 0) < 0) {
+				printf("error lseek() key: %s\n", strerror(errno));
+				exit(errno);
+			}
+		} else {
+			numkey += num;
+		}
+	}
+	// now we have 'numsrc' bytes - from both input and key file
+	// perform encryption operation
+	// (using srcbuf, no need for another buffer)
+	for (i = 0; i < numsrc; ++i)
+		srcbuf[i] = srcbuf[i] ^ keybuf[i];
+	close(fdkey);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	short port;
+	int fdkey, keylen, nread;
+	bool flag = true;
+	//initialize arsg
+	if (argc != 4 || argc != 3) {
+		printf("invalid number of arguments\n");
+		exit(-1);
+	}
+	port = (short) strtol(argv[2], NULL, 10);
+	if (port < 1) {
+		printf("port - invalid arguments\n");
+		exit(-1);
+	}
+	char* keyfilename = argv[2];
+	fdkey = open(keyfilename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fdkey < 0) {
+		printf("error open() output file %s: %s\n", argv[2], strerror(errno));
+		return errno;
+	}
+	if (argc == 4) {
+		keylen = strtol(argv[3], NULL, 10);
+		if (keylen < 1) {
+			printf("keylen - invalid arguments\n");
+			exit(-1);
+		}
+		if (create_key_file(fdkey, keylen) < 0) {
+			printf("error in creating key file\n");
+			exit(-1);
+		}
+	}
+	close(fdkey);
+
+	int totalsent, nsent, len, n = 0, listenfd = 0, connfd = 0;
+	struct sockaddr_in serv_addr, my_addr, peer_addr;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = any local machine address
+	serv_addr.sin_port = htons(port);
+
+	if (bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr))) {
+		printf("\n Error : Bind Failed. %s \n", strerror(errno));
+		exit(errno);
+	}
+
+	if (listen(listenfd, 10)) {
+		printf("\n Error : Listen Failed. %s \n", strerror(errno));
+		exit(errno);
+	}
+
+	while (flag) {
+		/* new connection */
+		socklen_t addrsize = sizeof(struct sockaddr_in);
+		connfd = accept(listenfd, (struct sockaddr*) &peer_addr, &addrsize);
+		if (connfd < 0) {
+			printf("\n Error : Accept Failed. %s \n", strerror(errno));
+			exit(errno);
+		}
+		int forked = fork();
+		if(forked == 0){
+			char srcbuf[BUF_SIZE];
+			memset(srcbuf, '0', sizeof(srcbuf));
+			nread = read(connfd, srcbuf, sizeof(srcbuf) - 1);
+					if (nread < 0) {
+						printf("error occured - read from client \n");
+						return -1;
+					}
+					if (xor_buffers(srcbuf, nread, fdkey) < 0) {
+						printf("error occured - xor buffers failed \n");
+						return -1;
+					}
+					totalsent = 0;
+					int notwritten = strlen(srcbuf);
+					/* keep looping until nothing left to write*/
+					while (notwritten > 0) {
+						/* notwritten = how much we have left to write
+						 totalsent  = how much we've written so far
+						 nsent = how much we've written in last write() call */
+						nsent = write(connfd, srcbuf + totalsent, notwritten);
+						if (nread != nsent) {
+							printf("error occured - write to client \n");
+							return -1;
+						}
+						totalsent += nsent;
+						notwritten -= nsent;
+					}
+		}
+		else if(forked < 0){
+			printf("error occured - forked failed \n");
+			return -1;
+		}
+
+		/* close socket  */
+		close(connfd);
+	}
+
+	return 0;
+}
+
